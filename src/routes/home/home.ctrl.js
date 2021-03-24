7f0c4ac2ccb8e1190e51fedef86c22a68a651ef4
choral7451 <choraels@naver.com>
2021-03-24 10:48:19 +0900

    프런트 로그인 기능 구현

---
+++ src/routes/home/home.ctrl.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const output = {
+    home: (req, res) => {
+        res.render("home/index"); 
+    },
+    login: (req, res) => {
+        res.render("home/login"); 
+    },
+};
+
+const users = {
+    id: ["nck", "nckmaster"],
+    psword: ["664258", "a740857"],
+};
+
+const process = {
+    login: (req, res) => {
+      const id = req.body.id,
+        psword = req.body.psword;
+
+      if (users.id.includes(id)) {
+        const idx = users.id.indexOf(id);
+          if (users.psword[idx] === psword) {
+              return res.json({
+                  success: true,
+              });
+          }
+      }
+
+      return res.json({
+          success: false,
+          msg: "로그인에 실패하셨습니다.",
+      });
+    }, 
+};
+
+module.exports = {
+   output,
+   process
+};
